3fc4ce318b9181db9c4e7e421b5d78994d43b3e9
Florian Schepers <florianschepers@gmx.de>
2023-08-23 14:30:11 +0200

    Add --openai Flag to main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ EXAMPLE_FILE_PATH = "examples/Scoreboard.wav"
 
 
 @log_time
-def create_meeting_minutes(audio_source, language):
+def create_meeting_minutes(audio_source, language, openai):
     diarization = diarize_audio(audio_source)
     save_diarization(diarization, "diarization.pkl")
 
@@ -22,7 +22,7 @@ def create_meeting_minutes(audio_source, language):
     save_transscript(audio_sections, transscript_sections, "transscript.txt")
 
     transscript = load_transscript("transscript.txt")
-    meeting_minutes = transscript_to_meeting_minutes(transscript, language)
+    meeting_minutes = transscript_to_meeting_minutes(transscript, language, openai)
     write_text_to_file(meeting_minutes, "meeting_minutes.txt")
     print(meeting_minutes)
 
@@ -42,10 +42,11 @@ def parse_input_file_path(input_file):
 
 @click.command()
 @click.option("-f", "--input-file")
-def main(input_file) -> None:
+@click.option("--openai", is_flag=True, show_default=True, default=False, help="If set Meminto will use OpenAis gpt-3.5-turbo as default LLM. Otherwise it will use LLM specified in enviroment variables.")
+def main(input_file, openai) -> None:
     audio_source = parse_input_file_path(input_file)
     language = "english"
-    create_meeting_minutes(audio_source, language)
+    create_meeting_minutes(audio_source, language, openai)
 
 
 if __name__ == "__main__":
