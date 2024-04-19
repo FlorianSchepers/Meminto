f7e49cdcca5cf62d59301256ed50d79e69d4c8df
Florian Schepers <florianschepers@gmx.de>
2024-04-19 21:45:43 +0200

    Refactor transcription
    Signed-off-by: Florian Schepers <florianschepers@gmx.de>

--- meminto/transcription.py
+++ meminto/transcription.py
@@ -3,7 +3,12 @@ from enum import Enum
 from pathlib import Path
 from typing import Any, Tuple
 import torch
-from transformers import WhisperProcessor, WhisperForConditionalGeneration
+from transformers import (
+    WhisperProcessor,
+    WhisperForConditionalGeneration,
+    pipeline,
+    Pipeline,
+)
 from audio_processing import SAMPLING_RATE, batch
 from decorators import log_time
 from helpers import Language
@@ -14,6 +19,7 @@ class WHISPER_MODEL_SIZE(Enum):
     BASE = "base"
     SMALL = "small"
     MEDIUM = "medium"
+    DISTIL = "distil"
     LARGE = "large"
 
 
@@ -28,13 +34,44 @@ class TranscriptSection:
         return f'{self.speaker}: "{self.text}"\n'
 
 
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
+@log_time
+def create_transcript(
+    audio_sections: list[Any], language: Language
+) -> list[TranscriptSection]:
+
+    print("Creating transcript from diarized audio")
+    whisper = setup_whisper()
+    transcript_sections = transcribe_sections(audio_sections, whisper)
+
+    return transcript_sections
 
 
 def setup_whisper(
     model_size: WHISPER_MODEL_SIZE = WHISPER_MODEL_SIZE.MEDIUM,
     english_only: bool = False,
-)->Tuple[WhisperProcessor, Any | WhisperForConditionalGeneration]:
+) -> Pipeline:
+    model_name = get_model_name(model_size, english_only)
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    print(f"Running on {device}")
+    whisper_processor = WhisperProcessor.from_pretrained(model_name)
+    whisper_model = WhisperForConditionalGeneration.from_pretrained(
+        model_name
+    ).to(device)
+    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+    whisper = pipeline(
+        "automatic-speech-recognition",
+        model=whisper_model,
+        tokenizer=whisper_processor.tokenizer,
+        feature_extractor=whisper_processor.feature_extractor,
+        max_new_tokens=128,
+        chunk_length_s=30,
+        batch_size=16,
+        torch_dtype=torch_dtype,
+        device=device,
+    )
+    return whisper
+
+def get_model_name(model_size: WHISPER_MODEL_SIZE, english_only: bool)->str:
     match model_size:
         case WHISPER_MODEL_SIZE.TINY:
             if english_only:
@@ -58,88 +95,35 @@ def setup_whisper(
                 )
             else:
                 whisper_model_name = "openai/whisper-medium"  # multilingual, ~ 3.06 GB
+        case WHISPER_MODEL_SIZE.DISTIL:
+            whisper_model_name = "distil-whisper/distil-large-v2"
         case WHISPER_MODEL_SIZE.LARGE:
             whisper_model_name = "openai/whisper-large-v2"  # multilingual, ~ 6.17 GB
         case _:
             whisper_model_name = "openai/whisper-medium"  # multilingual, ~ 3.06 GB
+    return whisper_model_name
 
-    whisper_processor = WhisperProcessor.from_pretrained(whisper_model_name)
-    whisper_model = WhisperForConditionalGeneration.from_pretrained(
-        whisper_model_name
-    ).to(device)
-    print(f"Running on {device}")
-    return whisper_processor, whisper_model
-
-
-def get_transcription_whisper(
-    audio,
-    whisper_processor,
-    whisper_model,
-    language="english",
-    skip_special_tokens=True,
-):
-    audio_batched = batch(audio, SAMPLING_RATE * 30)
-    transcription = []
-    forced_decoder_ids = whisper_processor.get_decoder_prompt_ids(
-        language=language.value, task="transcribe"
-    )
-    for section in audio_batched:
-        input_features = whisper_processor(
-            section, return_tensors="pt", sampling_rate=SAMPLING_RATE
-        ).input_features.to(device)
-
-        predicted_ids = whisper_model.generate(
-            input_features, forced_decoder_ids=forced_decoder_ids
-        )
-        transcription.append(
-            whisper_processor.batch_decode(
-                predicted_ids, skip_special_tokens=skip_special_tokens
-            )[0]
-        )
-    return "".join(transcription)
-
-
-def transcribe_audio_sections(
-    audio_sections: list[Any], language: Language
-) -> list[str]:
-    print("Creating transcript from diarized audio")
-    section_transcripts = []
-    (whisper_processor, whisper_model) = setup_whisper(
-        model_size=WHISPER_MODEL_SIZE.MEDIUM
-    )
+def transcribe_sections(audio_sections: list[Any], whisper: Pipeline) -> list[TranscriptSection]:
+        # transcribe sections
+    transcript_sections = []
     total_number_of_sections = len(audio_sections)
     for section_number, section in enumerate(audio_sections):
         print(f"Transscribing section {section_number} of {total_number_of_sections}.")
-        section_transcripts.append(
-            get_transcription_whisper(
-                section["audio"],
-                whisper_processor=whisper_processor,
-                whisper_model=whisper_model,
-                language=language,
-                skip_special_tokens=True,
-            )
+        transcription = whisper(
+            section["audio"].numpy(),
+            chunk_length_s=30,
+            stride_length_s=5,
+            batch_size=8,
         )
-    return section_transcripts
-
-
-@log_time
-def create_transcript(
-    audio_sections: list[Any], language: Language
-) -> list[TranscriptSection]:
-    transcript_sections = transcribe_audio_sections(audio_sections, language)
-
-    transcript = []
-    for idx, audio_section in enumerate(audio_sections):
-        transcript.append(
-            TranscriptSection(
-                start=audio_section["turn"].start,
-                end=audio_section["turn"].end,
-                speaker=audio_section["speaker"],
-                text=transcript_sections[idx].strip(),
+        print(transcription["text"])
+        transcript_section = TranscriptSection(
+                start=section["turn"].start,
+                end=section["turn"].end,
+                speaker=section["speaker"],
+                text=transcription["text"].strip(),
             )
-        )
-
-    return transcript
+        transcript_sections.append(transcript_section)
+    return transcript_sections
 
 
 def save_transcript_as_txt(transcript: list[TranscriptSection], file_name: Path):
